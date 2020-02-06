1a12c538d8fb18272adaf8fe74201f11b17e8093
Ridha <ridha.baoueb@instant-system.com>
2020-02-06 17:47:06 +0100

    first import swift frontend

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {first} from 'rxjs/operators';
+
+// @ts-ignore
+import {User} from '@app/_models';
+// @ts-ignore
+import {UserService, AuthenticationService} from '@app/_services';
+
+@Component({templateUrl: 'home.component.html'})
+export class HomeComponent implements OnInit {
+  loading = false;
+  users: User[];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.loading = true;
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.loading = false;
+      this.users = users;
+    });
+  }
+}
